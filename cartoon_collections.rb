2179421d6714170798cd4627538becff786f46a7
Sara A <calisaraalhindi@gmail.com>
2020-08-08 17:13:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   i= 0
   while i < dwarves.length
-  puts "#{i + 1}"
+  puts "#{i + 1}". "#{dwarves[i]}"
   dwarves.each.with_index 
   # code an argument here
   # Your code here
