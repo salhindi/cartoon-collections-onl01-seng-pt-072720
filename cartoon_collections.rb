1b254f8edc4c0a51c424613bed9e250e63a7a959
Sara A <calisaraalhindi@gmail.com>
2020-08-08 17:18:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def roll_call_dwarves(dwarves)
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  upcase_veggies = []
+  upcase_veggies << veggies.upcase
 end
 
 def long_planeteer_calls# code an argument here
